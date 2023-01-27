c52757344b83b866605cd5f8e223533f3b1d1154
= <linglingliu.to@gmail.com>
2023-01-27 22:04:27 +0000

    updated folder structure

---
+++ src/components/__tests__/App.test.js
@@ -0,0 +1,6 @@
+import { render, screen } from "@testing-library/react";
+import App from "./Application";
+
+test("renders learn react link", () => {
+  render(<App />);
+});
--- src/components/page.js
+++ src/components/page.js
@@ -1,7 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
+function Application() {
   return (
     <div className="App">
       <header className="App-header">
